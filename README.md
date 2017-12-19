68c40ed00402c85212175f657c8ffee06d3bedec
Ernesto Alejo <ernestokarim@gmail.com>
2017-12-19 17:13:56 +0100

    Open source application.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# find-unclosed-html-tag
-Small application to find unclosed HTML tags
+
+# static-html-linter
+
+> Custom HTML linter.
+
+HTMl linter customized to our clients needs. Backwards compatibility is not guaranteed. Use a copy of the code at your own risk.
+
+
+### Install
+
+```shell
+go get github.com/altipla-consulting/static-html-linter
+```
+
+
+### Usage
+
+```shell
+static-html-linter -file foo/bar.html
+```
+
+
+### Contributing
+
+You can make pull requests or create issues in GitHub. Any code you send should be formatted using ```gofmt```.
+
+
+### License
+
+[MIT License](LICENSE)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+var file = flag.String("file", "", "file to scan")
+
+type stackItem struct {
+	name string
+	raw  string
+	line int
+}
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Open(*file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	stack := []*stackItem{}
+
+	r := html.NewTokenizer(f)
+	currentLine := 1
+	for {
+		r.Next()
+
+		token := r.Token()
+		switch token.Type {
+		case html.ErrorToken:
+			log.Fatalf("token failed %s %#v", r.Err(), token)
+
+		case html.StartTagToken:
+			if isSelfClosed(token.Data) {
+				continue
+			}
+
+			stack = append(stack, &stackItem{
+				name: token.Data,
+				raw:  string(r.Raw()),
+				line: currentLine,
+			})
+
+		case html.EndTagToken:
+			expected := stack[len(stack)-1]
+			if expected.name != token.Data {
+				log.Println("Found unclosed tag!")
+				log.Println("===================")
+				log.Printf("Expected: </%s>\n", expected.name)
+				log.Printf("Got: </%s>\n", token.Data)
+				log.Printf("Opened in: %s", expected.raw)
+				log.Printf("Line: %d", expected.line)
+				os.Exit(1)
+				return
+			}
+			stack = stack[:len(stack)-1]
+		}
+		currentLine += strings.Count(string(r.Raw()), "\n")
+	}
+}
+
+func isSelfClosed(name string) bool {
+	switch name {
+	case "link":
+		return true
+	case "meta":
+		return true
+	case "img":
+		return true
+	}
+
+	return false
+}
